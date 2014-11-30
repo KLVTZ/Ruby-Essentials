5b66b69ab887efac3666ac483ea20d592db96c74
Justin Page <xjustinpagex@gmail.com>
2014-11-29 16:06:32 -0800

    update: chapter 06: Classes => init methods / classes ruby file / intro link test

--- sandbox/chapter_06/classes.rb
+++ sandbox/chapter_06/classes.rb
@@ -1,11 +1,33 @@
 class Animal
-	def make_noise
-		"Moo!"
+	attr_accessor :name
+	attr_writer :color
+	attr_reader :legs, :arms
+
+	def initialize
+		@legs = 4
+		@arms = 0
+		puts "A new animal has been instantiated."
+	end
+
+	def noise=(noise)
+		@noise = noise
+	end
+
+	def noise
+		@noise
+	end
+
+	def color
+		"The color is #{@color}."
 	end
 end
 
 animal1 = Animal.new
-puts animal1.make_noise
+animal1.noise = "Moo"
+animal1.name = "Bob"
+animal1.color = "black"
 
-animal2 = Animal.new
-puts animal2.make_noise
+puts animal1.color
+puts animal1.name
+puts animal1.noise
+puts animal1.legs
