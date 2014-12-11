7fddf0c4aff1be74f823fe89eef714b5907046aa
Justin Page <xjustinpagex@gmail.com>
2014-12-10 17:56:59 -0800

    update: modules mixins and load_require_and_include

--- sandbox/chapter_07/person.rb
+++ sandbox/chapter_07/person.rb
@@ -1,20 +1,7 @@
-module ContactInfo
-	attr_accessor :first_name, :last_name, :city, :state, :zip_code
-
-	def full_name
-		return @first_name + " " + @last_name
-	end
-
-	def city_state_zip
-		csz = @city
-		csz += ", #{@state}" if @state
-		csz += " #{@zip_code}" if @zip_code
-		return csz
-	end
-end
+require_relative 'contact_info'
 
 class Person
-	include ContactInfo
+	include ContactInfo # brings in modules
 end
 
 class Teacher
